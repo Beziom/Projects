9a6a6b7f6467c504da1529f81efdb8a36252e973
Beziom <Beziom@gmail.com>
2022-07-19 23:40:08 +0200

    Poprawa metody 'attack' oraz dopisanie docstringow

--- Python_gra/Gra.py
+++ Python_gra/Gra.py
@@ -1,71 +1,77 @@
-from abc import ABC, abstractmethod
-
 class Creature(object):
     """Basic model for creatures"""
 
-    atributes = []
+    def __init__(self, type:str, name:str, health:int, damage:int, defense:int, movement_speed:int):
+        """Function inits primary atributes for new creature
 
-    def __init__(self, type, name, health, damage, defense, movement_speed):
+        Args:
+            type (str): Describes creature's race
+            name (str): Creature's unique name
+            health (int): Points which desrbibe how much damage creature can take
+            damage (int): Number of health point which will be decresed from target
+            defense (int): How many damage creature will absorb
+            movement_speed (int): Number of spaces which creature can step on in 1 tour
+        """                      
         self.type = type
         self.name = name
         self.health = health
         self.damage = damage
         self.defense = defense
         self.movement_speed = movement_speed
+        self.is_alive = True
 
-    def die(self): # chciałbym aby po wywołaniu funkcji obiekt został usunięty/wyczyszcony z apmięci
-        print("Postać", self.name, "została zgładzona!")
+    def dead(self):
+        """Status that Creature cannot make any actions due to health points are 0"""        
+        print("Creature", self.name, "has been killed!")
+        self.is_alive = False
 
-    def is_alive(self):
+    def alive(self):
+        """Method which shows status of current health point"""        
         if self.health <= 0:
-            self.die()
+            self.dead()
         else:
-            print("Postać", self.name, "typu", self.type, "posiada", self.health, "punktów życia!")
+            print("Creature", self.name, "type", self.type, "has", self.health, "health points!")
     
-    def attack(self, target):
-        print(self.name, "atakuje", target.name, "oraz zadaje", self.damage, "obrażeń")
-        target.health = target.health -self.damage + target.defense
+    def attack(self,target:object):
+        """Creature's action which will decrease targets points of health
 
-    def defend(self, damage_taken): #czy robic ten atrybut na zasadzie bloku?
+        Args:
+            target (object): Creature on who attack action will affect
+        """                
+        if target.health >= 1:
+            target.health = target.health -self.damage + target.defense
+            print(self.name, "attacks", target.name, "and deals", self.damage - self.defense, "damage", "(" + str(self.defense), "blocked)")
+            if target.health <= 0:
+               target.dead()
+
+    def block(self, damage_taken):
         pass
 
-    @abstractmethod
     def statistic(self):
-        pass
+        a = ["Type:", "Name:", "Health:", "Damage:", "Defense:", "Movement_speed:"]
+        b = [self.type, self.name, self.health, self.damage, self.defense, self.movement_speed]
+        for i, j in zip(a, b): 
+            print(i,j)
  
-    @abstractmethod
     def position(self):
         pass
 
 class Demon(Creature):
     """Basic model for creature - Demon"""
 
-    def __init__(self, name, health, damage, defense, movement_speed): # Atrybuty są wpisywane będę myślał żeby zrobić losowy przedział
+    def __init__(self, name, health, damage, defense, movement_speed):
         super().__init__("Demon", name, health, damage, defense, movement_speed)
         self.atributes = ("Demon", name, health, damage, defense, movement_speed)
 
     def fireball(self):
         pass
 
-    def statistic(self):
-        a = ["Type:", "Name:", "Health:", "Damage:", "Defense:", "Movement_speed:"]
-        for i, j in zip(a, self.atributes): # Chciałbym stworzyć żeby były genrowany opisy statystyk
-            print(i,j)
-
 Demon1 = Demon("Krzsztof", 350, 35,20,5)
 Demon2 = Demon("Andrzej", 350, 35,20,5)
-Demon1.is_alive()
-print()
-Demon1.die()
-print()
-Demon1.statistic()
-print()
 print(Demon2.health)
-print()
 Demon1.attack(Demon2)
-print()
 print(Demon2.health)
-print()
 Demon1.attack(Demon2)
-print()
-print(Demon2.health)
+for i in range(22):
+    i = Demon1.attack(Demon2)
+Demon2.statistic()
